19d103d1e110c6b428d108fa2ce07d15fb8c4944
Shubham Gupta <shgupta222000@gmail.com>
2025-08-17 18:54:56 +0530

    Airplane Management

--- src/educative/AirLineManagement/Search.java
+++ src/educative/AirLineManagement/Search.java
@@ -1,4 +1,8 @@
 package educative.AirLineManagement;
 
+import java.util.Date;
+import java.util.List;
+
 public interface Search {
+    public List<FlightInstance> searchFlight(Airport source, Airport dest, Date arrival, Date departure);
 }
